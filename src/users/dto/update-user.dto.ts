0da654c7b9d06d91b13a36e10110a39efd35c77a
Vu Tung Lam <vutunglaminfo@gmail.com>
2025-09-16 07:23:20 +0700

    fix: Swagger not displaying fields of some DTOs

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
 
 export class CreateUserDto {
+    @ApiProperty()
     @IsOptional()
     @IsEmail(undefined, {
         message: "Email không hợp lệ.",
     })
     readonly email?: string;
 
+    @ApiProperty()
     @IsOptional()
     readonly full_name?: string;
 
+    @ApiProperty()
     @IsOptional()
     @MinLength(8, {
         message: "Mật khẩu phải có tối thiểu 8 ký tự.",
